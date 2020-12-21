ce8256ac1349f2c75fc790ef9bff248b9623e880
ShinyACash <76147605+ShinyACash@users.noreply.github.com>
2020-12-21 09:55:07 +0530

    avatar fix

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ client.on('messageDelete', async msg =>{
     .addField('Deleted by: ', msg.author.tag)
     .addField('Deleted from: ', msg.channel)
     .addField('Deleted msg: ', msg.content)
-    .addField('Who\'s msg was it?: ', msg.author.avatar)
+    .addField('Who\'s msg was it?: ', msg.author.displayAvatarURL)
     .setFooter('what a scrub');
 
     let deletelogging = client.channels.cache.get('775608981451702302');
