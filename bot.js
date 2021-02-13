0970920e2913eab1144c52b18d071171332d0bf6
ShinyACash <76147605+ShinyACash@users.noreply.github.com>
2021-02-13 20:16:07 +0530

    just for checking

--- bot.js
+++ bot.js
@@ -276,6 +276,7 @@ client.on('message', async(msg) => {
     if(dnd === true){
         var user = msg.mentions.users.first();
         if(user === "@ShinyStickmin"){
+            console.log("got dat mention!");
             if(msg.content.includes(user)){
                 let replies = ["ShinyStickmin-sama is currently busy, if he is REALLY needed then pls dm him. じゃあな! 😄", "He's busy, dm him if it's important.", "Look, sometimes a person can be busy, right now Shiny-sama is busy. DND ⛔", "Shiny-samaわ忙しいです. Use google translate if u want."];
                 let rand_replies = replies[Math.floor(Math.random() * replies.length)];
