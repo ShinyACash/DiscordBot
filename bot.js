a4b76b5eeb3b9907dae6eeeb85b7205fee8002e6
ShinyACash <76147605+ShinyACash@users.noreply.github.com>
2021-02-14 12:10:21 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -287,16 +287,16 @@ client.on('message', async(msg) => {
 
         console.log("check code here");
 
-        if(member.roles.find(r => r.name === 'Owner')) return dndtrigger = true;
+        if(member.roles.find(r => r.name === 'Owner')) return dndtrigger = true;*/
 
-        if(msg.isMen){
+        if(msg.mentions.users.first() === '695513111414964225'){
             if(dnd == true){
                 console.log("got dat mention!");
-            let replies = ["Shiny-sama is currently busy, if he is REALLY needed then pls dm him. じゃあな! 😄", "He's busy, dm him if it's important.", "Look, sometimes a person can be busy, right now Shiny-sama is busy. DND ⛔", "Shiny-sama わ忙しいです. Use google translate if u want.", "He busy, DESU!", "Probs watching anime or studying or something else ig.", "Not available...."];
-            let rand_replies = replies[Math.floor(Math.random() * replies.length)];
-            msg.channel.send(rand_replies);
+                let replies = ["Shiny-sama is currently busy, if he is REALLY needed then pls dm him. じゃあな! 😄", "He's busy, dm him if it's important.", "Look, sometimes a person can be busy, right now Shiny-sama is busy. DND ⛔", "Shiny-sama わ忙しいです. Use google translate if u want.", "He busy, DESU!", "Probs watching anime or studying or something else ig.", "Not available...."];
+                let rand_replies = replies[Math.floor(Math.random() * replies.length)];
+                msg.channel.send(rand_replies);
             }
-        }*/
+        }
         
     }
 
@@ -520,14 +520,6 @@ client.on('message', async(msg) => {
     
 });
 
-client.on("message", msg => {
-    msg.isMemberMentioned('695513111414964225') 
-    if(dnd == true){
-        let replies = ["Shiny-sama is currently busy, if he is REALLY needed then pls dm him. じゃあな! 😄", "He's busy, dm him if it's important.", "Look, sometimes a person can be busy, right now Shiny-sama is busy. DND ⛔", "Shiny-sama わ忙しいです. Use google translate if u want.", "He busy, DESU!", "Probs watching anime or studying or something else ig.", "Not available...."];
-        let rand_replies = replies[Math.floor(Math.random() * replies.length)];
-        msg.channel.send(rand_replies);
-    };
-})
 
 client.on('message', mg => {
 
