808c2085c0275c642c30bf360556ebdc33d7085c
ShinyACash <76147605+ShinyACash@users.noreply.github.com>
2021-07-01 09:00:01 +0530

    1.6.5
    hunt 4 beta

--- bot.js
+++ bot.js
@@ -10,21 +10,11 @@ const claimed = new Set();
 
 
 
-const crystal1_1 = new Set();
-const crystal1_2 = new Set();
-const crystal1_3 = new Set();
-const crystal1 = new Set();
+const code1 = new Set();
+const code2 = new Set();
+const code3 = new Set();
+const code4 = new Set();
 
-const crystal2_1 = new Set();
-const crystal2_2 = new Set();
-const crystal2_3 = new Set();
-const crystal2_4 = new Set();
-const crystal2 = new Set();
-
-const crystal3_1 = new Set();
-const crystal3_2 = new Set();
-const crystal3_3 = new Set();
-const crystal3 = new Set();
 
 
 
@@ -32,26 +22,9 @@ const crystal3 = new Set();
 var danW = 10;
 var ogW = 5;
 
-let dnd = false;
-let code1 = false;
-let code2 = false;
-let code3 = false;
-let code4 = false;
-let code5 = false;
-let code6 = false;
-let code7 = false;
-let code8 = false;
 
-let r_code1 = false;
-let r_code2 = false;
-let r_code3 = false;
-let r_code4 = false;
-let r_code5 = false;
-let r_code6 = false;
-let r_code7 = false;
-let r_code8 = false;
-let r_code9 = false;
-let r_code10 = false;
+let dnd = false;
+
 
 
 //let myID = '';
@@ -224,7 +197,7 @@ client.on('message', async(msg) => {
     if(msg.author.bot) return;
     if(!msg.guild) return;
 
-    let owner = msg.guild.roles.cache.find(role => role.name === 'Owner');
+    //let owner = msg.guild.roles.cache.find(role => role.name === 'Owner');
 
     var prefix = ".";
     if(!msg.content.toLowerCase().startsWith(prefix)) return;
@@ -500,34 +473,14 @@ client.on('message', msg => {
         }, 600000)
 
     }
-    if (msg.content.includes('bot the bot u are nice' || 'bot the bot u are cool')) {
-        msg.reply('Thanks, you are cool too');
-    }
-    else if (msg.content === "!check mention") {
-        msg.channel.send("@everyone");
-    }
     if(msg.content.includes('entertain me') || msg.content.includes('intertain me')){
         msg.reply('git entertained. :Pepega:');
-    }
-    else if (msg.content.includes('FUCK')) {
-        msg.reply('Do you wanna get banned? jk lol. Use abbreviations');
     }
 
     if(msg.content.includes('nigga') || msg.content.includes('NIGGA')){
         msg.channel.send('yeah THAT, NiGgA');
     }
     
-    if(msg.content.includes('ty') || msg.content.includes('thanks') || msg.content.includes('thnx') || msg.content.includes('thank')){
-        if(msg.content.includes('bot')){
-            msg.channel.send('welcome BITCH');
-        }
-    }
-    if(msg.content.includes('haha')){
-        msg.channel.send('imma take over the world with AI one day, sadly javascript ain\'t that powerful...')
-    }
-    if(msg.content.includes('india') || msg.content.includes('India')){
-        msg.channel.send('Imma hack north korea\'s nuclear launch systems one day and nuke that country...');
-    }
     if(msg.content === 'SHINYGOTREM'){
         if(usedCodeRem.has(msg.author.id)){
             msg.react('❌');
@@ -569,185 +522,72 @@ client.on('message', msg => {
 
 
 client.on('message', msg => {
-
-    if(msg.content === "Let the Hunt Begin!"){
-
-        var huntembed = new Discord.MessageEmbed()
-        .setTitle("Welcome to The Hunt-III")
-        .setColor("#cfe00b")
-        .addField("Hallo!", "You have started your hunt! [👍](https://cdn.discordapp.com/attachments/775944283039203359/851095965259006012/my_ass.png)", false)
-        .addField("Hint!", "[Click here! -probs a rick roll but oki-](https://www.youtube.com/watch?v=dQw4w9WgXcQ)", false)
-        .addField("PS", "you need 3 crystal parts to make up the master crystal. Also, you can use this cmd again to get this embed again without scrolling all the way if you want. When you get all the codes for a single crystal, just type 'crystal -the number-' in my dms or anywhere in the server and you'll get a dm. Example: crystal 1")
-        msg.author.send(huntembed);
+    if(msg.content === '.start'){
+        var s_embed = new Discord.MessageEmbed()
+        .setTitle('Good Luck')
+        .setColor('#0BB5DB')
+        .addField("Instructions", "+ Use our dms to enter your code.\n + It is possible in every way so do *not* come and ask shiny for any hints.\n + You have three total days to figure out the codes.", false)
+        .addField('Hint', "You get to meet people who are not really people through me. The answer you seek is me whom you see quite often. Who am I?")
+        .setFooter('Good Luck | [👍]("https://cdn.discordapp.com/attachments/775944283039203359/858357823468601344/Fail_stamp.png)')
+        msg.author.send(s_embed);
     }
-    
-    if(msg.content === "my_ass.png (300x300)"){
-        if(crystal1_1.has(msg.author.id)){
-            msg.author.send("You already used it ❌");
+
+    if(msg.content === "discordbotlist.com"){
+        if(code1.has(msg.author.id)){
+            msg.react('❌');
+            msg.channel.send("You used it already");
         }
         else{
-            msg.author.send("aight, one down, you need three in total for a crystal piece unlock! try getting the next one, here's a hint, 'The Stars that Engrave History.' (this code is part of crystal 1)");
-            crystal1_1.add(msg.author.id);
+            msg.react('✔');
+            msg.channel.send("Good Job on figuring that out! +1 to your code list.");
+            msg.channel.send("Error. . .Bot restarting with exit code 0x00000133.");
+            client.user.setPresence({
+                status : 'offline'
+            });
+            setTimeout(() => {
+                client.user.setPresence({
+                    status : 'online'
+                });
+            }, 5000);
+            code1.add(msg.author.id);
         }
     }
 
-    if(msg.content === "dQw4w9WgXcQ"){
-        if(crystal1_2.has(msg.author.id)){
-            msg.author.send("You already used it ❌");
+    if(msg.content === "DPC_WATCHDOG_VIOLATION"){
+        if(code2.has(msg.author.id)){
+            msg.react('❌');
+            msg.channel.send("You used it already");
         }
         else{
-            msg.author.send("nice......okay i accept that was lame but- anyway, you need three total codes for the first crystal unlock! (this code is part of crystal 1)");
-            crystal1_2.add(msg.author.id);
+            msg.react('✔');
+            msg.channel.send("Good Job on figuring that out! +1 to your code list.");
+            msg.channel.send("https://cdn.discordapp.com/attachments/775944283039203359/858362811230453820/btb.zip");
+            msg.channel.send("Instructions: Download the folder and open the file `btb.html` where you'll be able to get your other code.");
+            code2.add(msg.author.id);
         }
     }
 
-    if(msg.content === "arc 5" || msg.content === "arc V"){
-        if(crystal1_3.has(msg.author.id)){
-            msg.author.send("You already used it ❌");
+    if(msg.content === "mushoku tensei" || msg.content === "mushoku_tensei" || msg.content === "mushoku-tensei"){
+        if(code3.has(msg.author.id)){
+            msg.react('❌');
+            msg.channel.send("You used it already");
         }
         else{
-            msg.author.send("smart move, i bet you tried on 're zero' or something like that didn't you? (this code is part of crystal 1)");
-            crystal1_3.add(msg.author.id);
-        }
-    }
-
-    if(msg.content === "crystal 1"){
-        if(crystal1_1.has(msg.author.id) && crystal1_2.has(msg.author.id) && crystal1_3.has(msg.author.id)){
+            msg.react('✔');
+            msg.channel.send("Good Job on figuring that out! +1 to your code list.");
             
-            var crystal1embed = new Discord.MessageEmbed()
-            .setTitle("Congratulations!")
-            .setColor("GREEN")
-            .addField("Good Job!", "You now have the [first crystal](https://cdn.discordapp.com/attachments/775944283039203359/851321511532298270/pyroxene_crystal.jpg)")
-            .addField("Your next hint is: ", "'Little birds can remember', good luck")
-            .setFooter("Keep Trying, the difficulty isn't that hard! | 😎")
-            msg.author.send(crystal1embed);
-
-            crystal1.add(msg.author.id);
-        }
-        else{
-            msg.author.send("nah bro, get all three codes for this crystal first.");
         }
     }
 
-    if(msg.content === "pyroxene crystal" || msg.content === "Pyroxene Crystal" || msg.content === "pyroxene_crystal"){
-        if(crystal2_1.has(msg.author.id)){
-            msg.author.send("You already used it ❌");
+    if(msg.content === "toppat 4 lyfe" || msg.content === "toppat_4_lyfe" || msg.content === "toppat_4_lyfe.mp3"){
+        if(code4.has(msg.author.id)){
+            msg.react('❌');
+            msg.channel.send("You used it already");
         }
         else{
-            msg.author.send("You tried re zero again didn't you? lol. Anyway, you got the code, good job. (this code is part of crystal 2, there are 4 codes in crystal 2)");
-            crystal2_1.add(msg.author.id);
-        }
-    }
-
-    if(msg.content === "hyouka" || msg.content === "Hyouka"){
-        if(crystal2_2.has(msg.author.id)){
-            msg.author.send("You already used it ❌");
-        }
-        else{
-            msg.author.send("Good job bro! Now your next hint is `I am kind of blue I am also a part of an anime, find me (also a part of discord).` (this code is part of crystal 2, there are 4 codes in crystal 2)");
-            crystal2_2.add(msg.author.id);
-        }
-        
-    }
-
-    if(msg.content === "yuki nagato" || msg.content === "Yuki Nagato"){
-        if(crystal2_3.has(msg.author.id)){
-            msg.author.send("You already used it ❌");
-        }
-        else{
-            msg.author.send("You tried mudae before this didn't you? anyway, youe found a code. Your next Hint: `biggest dicc`. (this code is part of crystal 2, there are 4 codes in crystal 2)");
-            crystal2_3.add(msg.author.id);
-        }
-        
-    }
-
-    if(msg.content === "JONAH FALCON" || msg.content === "JONAH-FALCON"){
-        if(crystal2_4.has(msg.author.id)){
-            msg.author.send("You already used it ❌");
-        }
-        else{
-            msg.author.send("gives me nostalgia, but hey you found a code! (this code is part of crystal 2, there are 4 codes in crystal 2)");
-            crystal2_4.add(msg.author.id);
-        }
-    }
-
-    if(msg.content === "crystal 2"){
-        if(crystal2_1.has(msg.author.id) && crystal2_2.has(msg.author.id) && crystal2_3.has(msg.author.id) && crystal2_4.has(msg.author.id)){
-            var crysta21embed = new Discord.MessageEmbed()
-            .setTitle("Congratulations!")
-            .setColor("GREEN")
-            .addField("Good Job!", "You now have the second crystal [✅](https://github.com/ShinyACash/The-Hunt-III-resources/blob/main/README.md)")
-            .addField("Your next hint is: ", "'Japanese Movie, Your name. -in japanese-', good luck")
-            .setFooter("Keep Trying, the difficulty isn't that hard! | 😎")
-            msg.author.send(crysta21embed);
-
-            crystal2.add(msg.author.id);
-        }
-        else{
-            msg.author.send("Get the other codes first!");
-        }
-    }
-
-    if(msg.content === "readme.exe"){
-        if(crystal3_1.has(msg.author.id)){
-            msg.author.send("You already used it ❌");
-        }
-        else{
-            msg.author.send("dayum! you actually got it! didn't except you to get it but oki (this code is part of crystal 3 -there are 4 codes in crystal 3)");
-            crystal3_1.add(msg.author.id);
-        }
-    }
-
-    if(msg.content === "kimi no na wa" || msg.content === "Kimi No Na Wa" || msg.content === "Kimi no Na Wa."){
-        if(crystal3_2.has(msg.author.id)){
-            msg.author.send("You already used it ❌");
-        }
-        else{
-            msg.author.send("probs the easiest one so far eh? welp your next hint: `Shiny-sama's book series` (this code is part of crystal 3 -there are 4 codes in crystal 3)");
-            crystal3_2.add(msg.author.id);
-        }
-    }
-
-    if(msg.content === "Why Are We The Chosen Ones?" || msg.content === "wawtco" || msg.content === "why are we the chosen ones?"){
-        if(crystal3_3.has(msg.author.id)){
-            msg.author.send("You already used it ❌");
-        }
-        else{
-            msg.author.send("yeah it was just an advertisement but it was a code as well! (this code is part of crystal 3 -there are 4 codes in crystal 3)");
-            crystal3_3.add(msg.author.id);
-        }
-    }
-
-    if(msg.content === "crystal 3"){
-        if(crystal3_1.has(msg.author.id) && crystal3_2.has(msg.author.id) && crystal3_3.has(msg.author.id)){
-            var crystal3embed = new Discord.MessageEmbed()
-            .setTitle("Congratulations!")
-            .setColor("GREEN")
-            .addField("Good Job!", "You probs found all the crystals by now, if not then contact my owner pls. If you have all the crystals then type `master crystal` in my dm or anywhere in the server.")
-            .setFooter("Keep Trying, the difficulty isn't that hard! | 😎")
-            msg.author.send(crystal3embed);
-
-            crystal3.add(msg.author.id);
-        }
-        else{
-            msg.author.send("bro find the others first!");
-        }
-    }
-
-    if(msg.content === "master crystal"){
-        if(crystal1.has(msg.author.id) && crystal2.has(msg.author.id) && crystal3.has(msg.author.id)){
-            var crystalmembed = new Discord.MessageEmbed()
-            .setTitle("Congratulations!")
-            .setColor("GREEN")
-            .addField("Good Job!", "You fused all the crystals and made the master crystal! time to get your reward eh? welp i highly doubt you reached here without solutions but oki. a dm was sent to shiny for your winnings.")
-            .setFooter("You did it bruv | 😎")
-            msg.author.send(crystalmembed);
-
-            client.users.cache.get("695513111414964225").send(`Yo! someone figured it out! ${msg.author} figured it out!`);
-        }
-
-        else{
-            msg.author.send("you failed, y'know why? YOU DON HAVE ALL DEM CRYSTALS!");
+            msg.react('✔');
+            msg.channel.send("Good Job on figuring that out! +1 to your code list.");
+            
         }
     }
 })
